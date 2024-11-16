2ae23e73bd39540e501c2fd47eaceaa99005d656
Peter Svehla <psvehla@hotmail.com>
2024-11-16 21:24:22 +1100

    Tidy

--- consumer.py
+++ consumer.py
@@ -3,18 +3,20 @@ from json import loads
 
 # Hack while we wait for https://github.com/dpkp/kafka-python/issues/2412 to be resolved
 import six
-sys.modules['kafka.vendor.six.moves'] = six.moves
+
+sys.modules["kafka.vendor.six.moves"] = six.moves
 
 from kafka import KafkaConsumer
 
 
 consumer = KafkaConsumer(
-    'events', 
-    bootstrap_servers=['localhost:19092'], 
-    auto_offset_reset='earliest', 
-    enable_auto_commit=True, 
-    group_id='events', 
-    value_deserializer=lambda x: loads(x.decode('utf-8')))
+    "events",
+    bootstrap_servers=["localhost:19092"],
+    auto_offset_reset="earliest",
+    enable_auto_commit=True,
+    group_id="events",
+    value_deserializer=lambda x: loads(x.decode("utf-8")),
+)
 
 for message in consumer:
-    print(f'Received message: {message.value}')
+    print(f"Received message: {message.value}")
--- producer.py
+++ producer.py
@@ -4,15 +4,19 @@ from json import dumps
 
 # Hack while we wait for https://github.com/dpkp/kafka-python/issues/2412 to be resolved
 import six
-sys.modules['kafka.vendor.six.moves'] = six.moves
+
+sys.modules["kafka.vendor.six.moves"] = six.moves
 
 from kafka import KafkaProducer
 
 
-producer = KafkaProducer(bootstrap_servers=['localhost:19092'], value_serializer=lambda v: dumps(v).encode('utf-8'))
+producer = KafkaProducer(
+    bootstrap_servers=["localhost:19092"],
+    value_serializer=lambda v: dumps(v).encode("utf-8"),
+)
 
 for e in range(1000):
-    event = {'number': e}
+    event = {"number": e}
     producer.send("events", value=event)
     print(event)
     sleep(5)
